32e6918f970b329249a0697c9221c417bedcc171
Jeremy Chiang <jeremycpc2@gmail.com>
2018-04-23 16:27:48 +1000

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :computers do
+    resources :components, only: [:index]
+  end
+  resources :configurations, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
