88f63019f8d3006677635cc1879afb82f0ea07a3
Florence Schwartz <schflo@gmail.com>
2018-04-26 17:02:28 +1000

    fixed views and buttons

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -5,6 +5,7 @@ class ComponentsController < ApplicationController
     @computer = Computer.find(params["computer_id"])
     @spec = @computer.specs.find_by(category_id: @category.id)
 
+    @current_step = Spec::STEPS.index(@category.slug)
     @categories_left = Category.all.map(&:slug) - @spec.computer.completed_categories
   end
 
