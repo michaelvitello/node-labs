53c8c1c2c56b75c3e3db9e01cedcba0ea49d95ec
Jeremy Chiang <jeremycpc2@gmail.com>
2018-04-28 15:25:54 +1000

    Fixed update price in js

--- app/assets/javascripts/price_update.js
+++ app/assets/javascripts/price_update.js
@@ -1,9 +1,17 @@
 const componentPrices = document.querySelectorAll(".components");
 let totalPrice = document.getElementById("current-price");
 
+
+
 componentPrices.forEach(function(elem) {
   elem.addEventListener('click', function(e) {
-    total = Number(this.querySelector('p').getAttribute('data-value')) + Number(totalPrice.getAttribute('data-value'));
-    totalPrice.innerText = "$" + total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + '.00';
+    if (JSON.parse(totalPrice.getAttribute('data-set')) === null) {
+      total = Number(this.querySelector('p').getAttribute('data-value')) + Number(totalPrice.getAttribute('data-value'));
+      totalPrice.innerText = "$" + total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + '.00';
+    } else {
+      const previousSelection = JSON.parse(totalPrice.getAttribute('data-set')).price_cents/100;
+      total = Number(totalPrice.getAttribute('data-value')) - previousSelection + Number(this.querySelector('p').getAttribute('data-value'));
+      totalPrice.innerText = "$" + total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + '.00';
+    }
   });
 });
