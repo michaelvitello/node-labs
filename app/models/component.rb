e49f5f2f743f8b36da791555c902603b6375b12e
Harrison Malone <harrison.malone2@gmail.com>
2018-04-30 14:05:34 +1000

    styled the computer show page first steps

--- app/models/component.rb
+++ app/models/component.rb
@@ -2,4 +2,9 @@ class Component < ApplicationRecord
   has_many :specs
   belongs_to :category
   monetize :price_cents
+
+  def price_dollar_sign
+    return self.price ? "$#{self.price}" : ""
+  end
+
 end
