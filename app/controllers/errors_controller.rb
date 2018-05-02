402d24d8daeb9328b190f3a7f31bff6c641ab3ed
Florence Schwartz <schflo@gmail.com>
2018-05-02 11:55:14 +1000

    finished page 404

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,22 @@
+class ErrorsController < ApplicationController
+  layout 'error'
+
+  def show
+    status_code = params[:code] || 500
+
+    @images = [
+      "https://media.giphy.com/media/29SqSyXlyO6WI/giphy.gif",
+      "https://media.giphy.com/media/WyrdDeIxGOlQA/giphy.gif",
+      "https://media.giphy.com/media/ErXLlpllVNRoQ/giphy.gif",
+      "https://media.giphy.com/media/R35xeFYQ4NQTC/giphy.gif",
+      "https://media.giphy.com/media/3osxYACfOYULLSpNjG/giphy.gif",
+      "https://media.giphy.com/media/3ornkf5G08sz72snmM/giphy.gif",
+      "https://media.giphy.com/media/wkdSsTFRAwxcQ/giphy.gif",
+      "https://media.giphy.com/media/cjN7cKA8g0Qda/giphy.gif",
+      "https://media.giphy.com/media/Geu2l15OKUDEk/giphy.gif",
+    ]
+
+    render status_code.to_s, status: status_code
+  end
+
+end
