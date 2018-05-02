a5aea2d4af4d3df32bc2fcc58bcb513be23303ba
michael vitello <michael.vitello@gmail.com>
2018-05-02 12:05:09 +1000

    animation, pictures and pricing footer when buy

--- app/assets/javascripts/animation.js
+++ app/assets/javascripts/animation.js
@@ -1,18 +1,16 @@
-let frames = 11;
-let frame = -1;
+let frames = 26;
+let frame = 0;
 const div = document.getElementById("home-image");
 
-const getSpeed = function(){
-  return Number(document.getElementById('speed').value);
-}
+// const getSpeed = function(){
+//   return Number(document.getElementById('speed').value);
+// }
 
 const animate = function() {
-  setTimeout(function () {
+  setInterval(function () {
       let frameOffset = (++frame % frames);
       div.className = '';
-      div.classList.add("step-" + frameOffset)
-      animate();
-  }, getSpeed());
+      div.classList.add("img-" + frameOffset) }, 9);
 };
 
 document.addEventListener("DOMContentLoaded", animate);
