218432712d91749b5ab929c07ad811c748127375
Jeremy Chiang <jeremycpc2@gmail.com>
2018-05-03 12:34:15 +1000

    Made front-page responsive

--- app/assets/javascripts/animation.js
+++ app/assets/javascripts/animation.js
@@ -1,16 +1,16 @@
-let frames = 10;
-let frame = 0;
-const div = document.getElementById("home-image");
+// let frames = 10;
+// let frame = 0;
+// const div = document.getElementById("home-image");
 
-// const getSpeed = function(){
-//   return Number(document.getElementById('speed').value);
-// }
+// // const getSpeed = function(){
+// //   return Number(document.getElementById('speed').value);
+// // }
 
-const animate = function() {
-  setInterval(function () {
-      let frameOffset = (++frame % frames);
-      div.className = '';
-      div.classList.add("img-" + frameOffset) }, 12);
-};
+// const animate = function() {
+//   setInterval(function () {
+//       let frameOffset = (++frame % frames);
+//       div.className = '';
+//       div.classList.add("img-" + frameOffset) }, 12);
+// };
 
-document.addEventListener("DOMContentLoaded", animate);
+// document.addEventListener("DOMContentLoaded", animate);
