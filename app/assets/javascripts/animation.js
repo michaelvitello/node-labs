ac9b38e2e74952f24c3c2668ffd91b9726fce39c
Cyrille Labesse <cyrille@inomads.hk>
2018-05-03 16:18:48 +1000

    Crazy animations

--- app/assets/javascripts/animation.js
+++ app/assets/javascripts/animation.js
@@ -1,14 +1,31 @@
 let frames = 10;
 let frame = 0;
+let speed = 12;
+
 const div = document.getElementById("home-image");
 
-// // const getSpeed = function(){
-// //   return Number(document.getElementById('speed').value);
-// // }
+const pullThePlug = function() {
+  speed = speed * (1 + speed / 1000);
+
+  if (speed > 1000) {
+    div.removeEventListener('click', pullThePlug, true);
+    div.addEventListener('click', pushThePlug);
+  }
+  else {
+    setTimeout(pullThePlug, speed);
+  }
+};
+
+const pushThePlug = function() {
+  speed = 12;
+  animate();
+};
 
 const animate = function() {
-  setInterval(function () {
-      let frameOffset = (++frame % frames);
-      div.className = '';
-      div.classList.add("img-" + frameOffset) }, 12);
-}
+  div.className = '';
+  div.classList.add("img-" + (++frame % frames))
+
+  if (speed < 1000) setTimeout(animate, speed);
+
+  div.addEventListener('click', pullThePlug);
+};
