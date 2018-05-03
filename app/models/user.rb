4af2c13165210c3d60beb63e3a16cf1b84f4c35b
Florence Schwartz <schflo@gmail.com>
2018-05-03 12:28:11 +1000

    added twitch connect

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, omniauth_providers: [:twitch]
+
+  def self.from_omniauth(auth)
+    where(uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.token = auth.credentials.token
+    end
+  end
 end
